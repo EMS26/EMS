804a0881022f8e5bc2bd4b6887c2d02a913d52e8
sumanan24 <sumanan24@gmail.com>
2021-04-19 13:42:53 +0530

    first commit

--- admin/photographpackage.php
+++ admin/photographpackage.php
@@ -66,7 +66,7 @@
                                             <div class="col-md-9"><input type="text" class="form-control" name="album" required></div>
                                         </div>
                                     </div>
-                            </div>
+                               </div>
                             <div class="card-footer ">
                                 <div class="row">
                                     <div class="col"></div>
