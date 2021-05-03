492c0a190af51b6d8dd1348177ff5f9daa1f4a84
sumanan24 <sumanan24@gmail.com>
2021-05-03 14:16:54 +0530

    edit

--- admin/hall.php
+++ admin/hall.php
@@ -8,11 +8,44 @@
 <head>
     <link rel="stylesheet" href="css/style.css">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
 </head>
 
 <body>
+    <?php
+$provider_id=null;
+$hallname=null;
+$capacity=null;
+$rentac=null;
+$rentnonac=null;
+$advanced=null;
+$provider_name=null;
+if(isset($_GET['edit']))
+{
+    $id=$_GET['edit'];
+    $sql="select * from hall where Hall_id=$id";
+    $result=mysqli_query($con,$sql);
+    if($row=mysqli_fetch_assoc($result))
+    {
+        $provider=$row['Provider_id'];
+        $hallname=$row['Hall_name'];
+        $capacity=$row['Hall_capacity'];
+        $rentac=$row['Rent_with_ac'];
+        $rentnonac=$row['Rent_without_ac'];
+        $advanced=$row['Advanced'];
+    }
+
+    $sql="select * from providers where Provider_id=$provider";
+    $result=mysqli_query($con,$sql);
+    if($row=mysqli_fetch_assoc($result))
+    {
+        $provider_name=$row['Provider_name'];
+    }
+
+}
+?>
     <div class="page-wrapper chiller-theme toggled">
         <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
             <i class="fas fa-bars"></i>
@@ -47,11 +80,11 @@
                                     $sql = "insert into hall(Provider_id,Hall_name,Hall_capacity,Rent_with_ac,Rent_without_ac,Advanced) values ($provider,'$hallname',$capacity,$rentac,$rentnonac,$advanced)";
                                     if (mysqli_query($con, $sql)) {
                                 ?>
-                                        <div class="alert alert-success" role="alert">
-                                            Insert sucessfully!
-                                            <?php echo "<meta http-equiv='refresh' content='0.6'>"; ?>
-                                        </div>
-                                        <?php
+                                <div class="alert alert-success" role="alert">
+                                    Insert sucessfully!
+                                    <?php echo "<meta http-equiv='refresh' content='0.6'>"; ?>
+                                </div>
+                                <?php
                                     }
                                     else
                                     {
@@ -69,18 +102,43 @@
                                                     </b></label></div>
                                             <div class="col-md-9 ">
                                                 <select name="provider" id="" class="form-control">
+                                                    <?php if(isset($_GET['edit']))
+                                                    {
+                                                        ?>
+                                                    <option value="<?php echo $provider;?>" selected>
+                                                        <?php echo $provider_name;?></option>
+                                                    <?php
+                                                    }
+                                                    else
+                                                    {
+                                                     ?>
                                                     <option value="" selected disabled>Select provider</option>
                                                     <?php
+                                                    }
+                                                    ?>
+
+                                                    <?php
+                                                    
                                                     $sql = 'SELECT * FROM `providers`';
                                                     $result = mysqli_query($con, $sql);
                                                     if (mysqli_num_rows($result) > 0) {
                                                         while ($row = mysqli_fetch_assoc($result)) {
+                                                            if($provider_name==$row['Provider_name'])
+                                                            {
+
+                                                            }
+                                                            else
+                                                            {
                                                     ?>
-                                                            <option value="<?php echo $row['Provider_id']; ?>" style="text-transform: capitalize;">
-                                                                <?php echo $row['Provider_name']; ?></option>
+
+                                                    <option value="<?php echo $row['Provider_id']; ?>"
+                                                        style="text-transform: capitalize;">
+                                                        <?php echo $row['Provider_name']; ?></option>
                                                     <?php
                                                         }
                                                     }
+                                                    }
+                                                    
                                                     ?>
                                                 </select>
                                             </div>
@@ -90,7 +148,9 @@
                                         <div class="row">
                                             <div class="col-md-3" style="text-align: right;"> <label> <b>Hall Name
                                                     </b></label></div>
-                                            <div class="col-md-9 "> <input type="text" name="hallname" class="form-control" required></div>
+                                            <div class="col-md-9 "> <input type="text" name="hallname"
+                                                    class="form-control" required
+                                                    value="<?php if(isset($_GET['edit'])){echo $hallname;}?>"></div>
                                         </div>
                                     </div>
                                     <div class="form-group">
@@ -98,7 +158,9 @@
                                             <div class="col-md-3" style="text-align: right;">
                                                 <label><b>Capacity</b></label>
                                             </div>
-                                            <div class="col-md-9"><input type="number" class="form-control" name="capacity" required></div>
+                                            <div class="col-md-9"><input type="number" class="form-control"
+                                                    name="capacity" required
+                                                    value="<?php if(isset($_GET['edit'])){echo $capacity;}?>"></div>
                                         </div>
                                     </div>
 
@@ -106,29 +168,50 @@
                                         <div class="row">
                                             <div class="col-md-3" style="text-align: right;"> <label><b>Rent With
                                                         AC</b></label></div>
-                                            <div class="col-md-9"><input type="number" class="form-control" name="rentac" required></div>
+                                            <div class="col-md-9"><input type="number" class="form-control"
+                                                    name="rentac" required
+                                                    value="<?php if(isset($_GET['edit'])){echo $rentac;}?>"></div>
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <div class="row">
                                             <div class="col-md-3" style="text-align: right;"> <label><b>Rent With Non
                                                         AC</b></label></div>
-                                            <div class="col-md-9"><input type="number" class="form-control" name="rentnonac" required></div>
+                                            <div class="col-md-9"><input type="number" class="form-control"
+                                                    name="rentnonac" required
+                                                    value="<?php if(isset($_GET['edit'])){echo $rentnonac;}?>"></div>
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <div class="row">
                                             <div class="col-md-3" style="text-align: right;"> <label><b>Advanced
                                                         Amount</b></label></div>
-                                            <div class="col-md-9"><input type="number" class="form-control" name="advanced" required></div>
+                                            <div class="col-md-9"><input type="number" class="form-control"
+                                                    name="advanced" required
+                                                    value="<?php if(isset($_GET['edit'])){echo $advanced;}?>"></div>
                                         </div>
                                     </div>
                             </div>
                             <div class="card-footer ">
                                 <div class="row">
                                     <div class="col"></div>
-                                    <div class="col-auto"><input type="Reset" value="Reset" class="btn btn-primary">
+                                    <div class="col-auto">
+                                        <?php 
+                                          if(isset($_GET['edit']))
+                                          {
+                                              ?>
+                                        <input type="Submit" value="Update" class="btn btn-primary">
+                                        <?php
+                                          }
+                                          else
+                                          {
+                                              ?>
+                                        <input type="Reset" value="Reset" class="btn btn-primary">
                                         <input type="submit" value="Add hall" name="submit" class="btn btn-success">
+                                        <?php
+                                          }
+                                          ?>
+
                                     </div>
                                 </div>
                             </div>
@@ -144,9 +227,11 @@
     </div>
     <!-- page-wrapper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 
 </body>
